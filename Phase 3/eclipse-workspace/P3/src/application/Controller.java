a680778bb2c172f9beac47889bf47678970edb2b
Vihaan Patel <vihaan004@gmail.com>
2024-04-08 21:43:34 -0700

    new view object for portals on logging in again

--- Phase 3/eclipse-workspace/P3/src/application/Controller.java
+++ Phase 3/eclipse-workspace/P3/src/application/Controller.java
@@ -1,3 +1,53 @@
+//package application;
+//
+//import javafx.scene.Scene;
+//import javafx.stage.Modality;
+//import javafx.stage.Stage;
+//
+//public class Controller {
+//	LoginView loginView;
+//	NurseView nurseView;
+//	DoctorView doctorView;
+//	PatientView patientView;
+//	
+//	Controller (Stage stage, int width, int height) {
+//		loginView = new LoginView(stage, this, width, height);
+//		nurseView = new NurseView(stage, this, width, height);
+//		doctorView = new DoctorView(stage, this, width, height);
+//		patientView = new PatientView(stage, this, width, height);
+//	}
+//	
+//	Controller () {
+//		
+//	}
+//	
+//	public void appStart() {
+//		loginView.show();
+//	}
+//	
+//	public void showNurseView() {
+//		nurseView.show();
+//	}
+//	
+//	public void showDoctorView() {
+//		doctorView.show();
+//	}
+//	
+//	public void showPatientView(Patient patient) {
+//		patientView.show(patient);
+//	}
+//	
+//	public void popup(Stage parentStage, Scene scene, String title) {
+//		Stage popup = new Stage();
+//		popup.initOwner(parentStage);
+//		popup.initModality(Modality.WINDOW_MODAL);
+//        popup.setTitle(title);	
+//        popup.setScene(scene);
+//        popup.show();
+//	}
+//}
+
+
 package application;
 
 import javafx.scene.Scene;
@@ -5,35 +55,37 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Controller {
+	
+	Stage stage;
+	int width, height;
 	LoginView loginView;
 	NurseView nurseView;
 	DoctorView doctorView;
 	PatientView patientView;
 	
 	Controller (Stage stage, int width, int height) {
-		loginView = new LoginView(stage, this, width, height);
-		nurseView = new NurseView(stage, this, width, height);
-		doctorView = new DoctorView(stage, this, width, height);
-		patientView = new PatientView(stage, this, width, height);
-	}
-	
-	Controller () {
-		
+		this.stage = stage;
+		this.width = width;
+		this.height = height;
 	}
 	
 	public void appStart() {
+		loginView = new LoginView(stage, this, width, height);
 		loginView.show();
 	}
 	
 	public void showNurseView() {
+		nurseView = new NurseView(stage, this, width, height);
 		nurseView.show();
 	}
 	
 	public void showDoctorView() {
+		doctorView = new DoctorView(stage, this, width, height);
 		doctorView.show();
 	}
 	
 	public void showPatientView(Patient patient) {
+		patientView = new PatientView(stage, this, width, height);
 		patientView.show(patient);
 	}
 	
