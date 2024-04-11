5f2cbd0f0e56b7a8e42e3e5b83f1a6754e14df5b
Ayush Shejwal <ayushshejwal@Ayushs-MacBook-Air.local>
2024-04-10 18:44:32 -0700

    changes

--- Phase 3/eclipse-workspace/P3/src/application/Patient.java
+++ Phase 3/eclipse-workspace/P3/src/application/Patient.java
@@ -262,6 +262,7 @@ public class Patient {
     
     public void saveHealthRecord(String record) {
     	LocalDateTime now = LocalDateTime.now();
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh-mm a_MM-dd-yyyy");
     }
 
     
